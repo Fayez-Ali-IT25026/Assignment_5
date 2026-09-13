4dab87546ea5bf3c8c3339d7e43c2e1438b1611c
Fayez-Ali-IT25026 <fayezali210906@gmail.com>
2026-09-13 19:56:31 +0600

    footer 1

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,8 +4,8 @@ import BannerImage from "../assets/banner-stack.png";
 const Banner = () => {
     return (
         <div>
-            <div className="flex justify-between items-center container mx-auto px-6 ">
-                <div className="pl-30">
+            <div className="flex justify-between items-center container mx-auto px-20 ">
+                <div className="pl-5">
                          <h2 className="text-5xl font-bold"> Build Your Ideal <br /><span className="bg-gradient-to-r from-red-500 via-pink-500 to-purple-600 bg-clip-text text-transparent">Development Stack</span></h2>
                 <p className="mt-5 text-gray-600 max-w-lg leading-6">Explore frontend, backend, database, and tooling options,compare them side by side, and put together the stack that fits yournext project.</p>
                 <div className="flex  gap-3 pt-3">
--- src/components/Fotter.tsx
+++ src/components/Fotter.tsx
@@ -1,10 +1,10 @@
 import Logo from '../assets/logo-text.png'
 const Fotter = () => {
     return (
-        <div className='flex justify-between items-center container mx-auto'>
+        <div className='flex justify-between items-center container mx-auto px-15'>
             <div>
                 <img src={Logo} alt="" />
-                <p>Curated tools, technologies, and resources for developers building <br />
+                <p className='text-[#64748B] py-5'>Curated tools, technologies, and resources for developers building <br />
 modern software.</p>
 <div >
     <ul className='flex gap-3'>
@@ -17,20 +17,20 @@ modern software.</p>
             </div>
             <div>
                 <h2>PRODUCT</h2>
-                <p><a href="">Home</a></p>
-                <p><a href="">Technologies</a></p>
-                <p><a href="">Projects</a></p>
+                <p className='text-[#64748B]'><a href="">Home</a></p>
+                <p className='text-[#64748B]'><a href="">Technologies</a></p>
+                <p className='text-[#64748B]'><a href="">Projects</a></p>
             </div>
             <div>
                 <h2>COMPANY</h2>
-                <p><a href="">About</a></p>
-                <p><a href="">Contact</a></p>
-                <p><a href="">Careers</a></p>
+                <p className='text-[#64748B]'><a href="">About</a></p>
+                <p className='text-[#64748B]'><a href="">Contact</a></p>
+                <p className='text-[#64748B]'><a href="">Careers</a></p>
             </div>
             <div>
                 <h2>LEGAL</h2>
-                <p><a href="">Privacy Policy</a></p>
-                <p><a href="">Terms of Service</a></p>
+                <p className='text-[#64748B]'><a href="">Privacy Policy</a></p>
+                <p className='text-[#64748B]'><a href="">Terms of Service</a></p>
                 
             </div>
         </div>
