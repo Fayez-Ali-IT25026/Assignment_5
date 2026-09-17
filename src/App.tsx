786e440563a8bed224cd030df9bc7a951858d7e3
Fayez-Ali-IT25026 <fayezali210906@gmail.com>
2026-09-17 17:23:13 +0600

    devsection part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import Nav from './components/Nav'
 import Banner from './components/Banner'
 import Fotter from './components/Fotter'
+import type { promises } from './dev types/devStackType';
+import dev_stack from './components/devstack/dev_stack'
+import { Suspense } from 'react';
 
-const devfetch = async () => 
+
+const devfetch = async ():promises<MydevStack[]> => 
 {
   const res = await fetch ("/data.json");
   const data = await res.json();
+   return data;
 }
 
 function App() {
 
-  const devPromise = devfetch
+  const devPromise = devfetch();
   return (
     <>
    <Nav />
    <Banner/>
    <Fotter/>
-   <suspense>
-    
-   </suspense>
+   <Suspense fallback = {<h2>Loading .....</h2>}>
+    <dev_stack devPromise={devPromise} />
+   </Suspense>
     </>
   )
 }
---
+++ src/components/devstack/DevStack.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const dev_stack = ({devPromise}) => {
+    console.log(devPromise)
+const devStack = use(devPromise)
+    console.log(devStack,"devStack")    
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default dev_stack;
