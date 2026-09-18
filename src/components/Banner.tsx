a9078ec871c9893e9e84add82cc50d8bc0cad430
Fayez-Ali-IT25026 <fayezali210906@gmail.com>
2026-09-18 18:19:00 +0600

    Modify

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,9 +4,9 @@ import BannerImage from "../assets/banner-stack.png";
 const Banner = () => {
     return (
         <div>
-            <div className="flex justify-between items-center container mx-auto px-20 ">
+           <div className="flex flex-col md:flex-row justify-between items-center container mx-auto px-5 md:px-10 lg:px-20">
                 <div className="pl-5">
-                         <h2 className="text-5xl font-bold"> Build Your Ideal <br /><span className="gradient-primary bg-clip-text text-transparent">Development Stack</span></h2>
+                         <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold"> Build Your Ideal <br /><span className="gradient-primary bg-clip-text text-transparent">Development Stack</span></h2>
                 <p className="mt-5 text-gray-600 max-w-lg leading-6">Explore frontend, backend, database, and tooling options,compare them side by side, and put together the stack that fits yournext project.</p>
                 <div className="flex  gap-3 pt-3">
   <button className="btn gradient-primary text-white border-0">
@@ -22,7 +22,11 @@ const Banner = () => {
 
             
             <div>
-                <img src={BannerImage} alt="" />
+               <img
+    src={BannerImage}
+    alt=""
+    className="w-[300px] md:w-[400px] h-auto"
+/>
             </div>
             </div>
 
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,29 +2,43 @@ import Logo from '../assets/logo-text.png'
 
 const Nav = () => {
   return (
-    <nav className="flex items-center justify-between px-15 py-5 bg-white container mx-auto">
-      
-      <div>
-        <img src={Logo} alt="Dev Stack Logo" className="w-40" />
-      </div>
+    <nav className="sticky top-0 z-50 bg-white">
+    <div className="container mx-auto flex items-center justify-between px-5 py-5 md:px-15">
 
-      <div>
-        <ul className="flex gap-8">
-          <li className='text-[#DB2777]'><a href="http://">Home</a> </li>
-          <li><a href="http://">Technologies</a></li>
-          <li><a href="">Projects</a></li>
-          
-          <li><a href="">About</a></li>
-          <li><a href="">Contact</a></li>
-        </ul>
-      </div>
+        <button className="md:hidden text-2xl">
+            ☰
+        </button>
 
-      <div className="flex gap-3">
-        <button className="btn rounded-2xl">Sign In</button>
-        <button className="btn rounded-2xl bg-[#D91B7E] text-white">Sign Up</button>
-      </div>
+        <div>
+            <img
+                src={Logo}
+                alt="Dev Stack Logo"
+                className="w-32 md:w-40"
+            />
+        </div>
 
-    </nav>
+        <div className="hidden md:block">
+            <ul className="flex gap-8">
+                <li className="text-[#DB2777]"><a href="">Home</a></li>
+                <li><a href="">Technologies</a></li>
+                <li><a href="">Projects</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </div>
+
+        <div className="flex gap-2 md:gap-3">
+            <button className="btn rounded-2xl btn-sm md:btn-md">
+                Sign In
+            </button>
+
+            <button className="btn rounded-2xl bg-[#D91B7E] text-white btn-sm md:btn-md">
+                Sign Up
+            </button>
+        </div>
+
+    </div>
+</nav>
   )
 }
 
