e58662cb6296cd43ea2b7eda8f265512574ecab0
Fayez-Ali-IT25026 <fayezali210906@gmail.com>
2026-09-21 01:16:36 +0600

    Fix git 1.2

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Assignment_5/',
+  // base: '/Assignment_5/',
 })
